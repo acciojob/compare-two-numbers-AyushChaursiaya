fa3005efa47c1f635057734a2bccb6532e4bfd3e
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-08-10 19:18:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let num1 = prompt("Enter a first number");
+let num2 = prompt("Enter a second number"); 
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+let areEqule = num1 === num2;
+alert(areEqule);
